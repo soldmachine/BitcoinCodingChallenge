9885696da93474a9c35c8b0445a898fd5dcdbd74
Thomas Szoldatits <tszoldatits@cellular.de>
2019-06-01 11:50:54 +0200

    Merge layouts into activity_main (as this is just a simple app) + extracted loading view

--- app/src/main/java/com/szoldapps/bitcoin/ui/main/MainActivity.kt
+++ app/src/main/java/com/szoldapps/bitcoin/ui/main/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.szoldapps.bitcoin.ui.main
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -14,8 +11,7 @@ import com.github.mikephil.charting.data.LineDataSet
 import com.szoldapps.bitcoin.R
 import com.szoldapps.bitcoin.databinding.ActivityMainBinding
 import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.main.activity_main.toolbar
-import kotlinx.android.synthetic.main.content_main.chart
+import kotlinx.android.synthetic.main.activity_main.chart
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -33,16 +29,7 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.state = mainViewModel.state
         binding.lifecycleOwner = this
-        setSupportActionBar(toolbar)
 
-
-        mainViewModel.state.observe(this, Observer { mainState ->
-            when (mainState) {
-                MainActivityState.SHOW_CHART -> chart.visibility = View.VISIBLE
-                else -> chart.visibility = View.GONE
-            }
-
-        })
         mainViewModel.marketPriceData.observe(this, Observer { marketPriceData ->
             chart.apply {
                 axisRight.isEnabled = false
@@ -57,19 +44,4 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.loadMarketPriceData()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
